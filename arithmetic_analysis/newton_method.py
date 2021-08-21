089b0ac071eb5bd1b20cb40f7d71f2ecd54ac11a
slowy07 <slowy.arfy@gmail.com>
2021-08-21 11:27:48 +0700

    add: menambahkan algoritma teori newton method

---
+++ arithmetic_analysis/newton_method.py
@@ -0,0 +1,50 @@
+# metode newton https://en.wikipedia.org/wiki/Newton%27s_metho
+from typing import Callable
+
+function_real = Callable[[float], float]
+
+
+def newton(function: function_real, deriative: function_real, starting_int: int) -> int:
+    """
+    >>> newton(lambda x: x ** 3 - 2 * x - 5, lambda x: 3 * x ** 2 - 2, 3)
+    2.0945514815423474
+    >>> newton(lambda x: x ** 3 - 1, lambda x: 3 * x ** 2, -2)
+    1.0
+    >>> newton(lambda x: x ** 3 - 1, lambda x: 3 * x ** 2, -4)
+    1.0000000000000102
+    >>> import math
+    >>> newton(math.sin, math.cos, 1)
+    0.0
+    >>> newton(math.sin, math.cos, 2)
+    3.141592653589793
+    >>> newton(math.cos, lambda x: -math.sin(x), 2)
+    1.5707963267948966
+    >>> newton(math.cos, lambda x: -math.sin(x), 0)
+    Traceback (most recent call last):
+    ...
+    ZeroDivisionError: tidak dapat menemukan akar
+    """
+    prev_guess = float(starting_int)
+    while True:
+        try:
+            next_guess = prev_guess - function(prev_guess) / deriative(prev_guess)
+        except ZeroDivisionError:
+            raise ZeroDivisionError("tidak dapat menemukan akar") from None
+        if abs(prev_guess - next_guess) < 10 ** -5:
+            return next_guess
+        prev_guess = next_guess
+
+
+def f(x: float) -> float:
+    return (x ** 3) - (2 * x) - 5
+
+
+def f1(x: float) -> float:
+    return 3 * (x ** 2) - 2
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    # print(newton(f, f1, 3))
