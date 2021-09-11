efa1d1d2ba8ddd12da031fcb3127ec38e28499a7
KelvinMulyawan <kelvinmulyawan2000@gmail.com>
2021-09-11 19:17:45 +0700

    Update Introduction.py

--- Basic/Introduction.py
+++ Basic/Introduction.py
@@ -1,1 +1,3 @@
-print("Hello World )
+# Pada bahasa pemrograman Python, kita cukup memakai perintah print untuk mendapatkan output dari apa yang kita inginkan
+# Misalnya, output yang diinginkan adalah Turki, maka kita mengetikkan Turki, seperti dibawah ini
+print("Negara Turki")
