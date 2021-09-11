4726d736a5e54b5ffdc30d1de31074bfbbe28673
arfy slowy <slowy.arfy@gmail.com>
2021-09-11 19:29:12 +0700

    fix: issue pada flake8

--- Basic/Introduction.py
+++ Basic/Introduction.py
@@ -1,3 +1,6 @@
-# Pada bahasa pemrograman Python, kita cukup memakai perintah print untuk mendapatkan output dari apa yang kita inginkan
-# Misalnya, output yang diinginkan adalah Turki, maka kita mengetikkan Turki, seperti dibawah ini
+# Pada bahasa pemrograman Python, kita cukup
+# memakai perintah print untuk mendapatkan
+# output dari apa yang kita inginkan
+# Misalnya, output yang diinginkan adalah Turki,
+# maka kita mengetikkan Turki, seperti dibawah ini
 print("Negara Turki")
