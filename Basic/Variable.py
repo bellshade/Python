46c42a18c3e5a92dcc84e79b22084658507e48ea
slowy07 <slowy.arfy@gmail.com>
2021-09-12 21:17:15 +0700

    add: contoh variable
    folder: basic
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ Basic/Variable.py
@@ -0,0 +1,12 @@
+# variable pada python juga
+# dan juga banyak data dan value pada python
+# contoh dasar yaitu int, float, str
+
+nama = "Riskiy"  # contoh tipe data string
+umur = 12  # contoh tipe data integer (int)
+nilai = 95.5  # nilai
+
+print("Dia bernama ", nama)
+print("Dia baru berumur ", umur," tahun")
+print("dan dia mendapatkan nilai ", nilai)
+print("dan itu sangat hebat sekali")
