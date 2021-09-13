3b8e082c16e1146e0aaa8a140ed0b99574223d01
slowy07 <slowy.arfy@gmail.com>
2021-09-13 22:06:48 +0700

    fix: menambahkan basic variabel
    folder: Basic
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- Basic/Variable.py
+++ Basic/Variable.py
@@ -7,6 +7,27 @@ umur = 12  # contoh tipe data integer (int)
 nilai = 95.5  # nilai
 
 print("Dia bernama ", nama)
-print("Dia baru berumur ", umur," tahun")
+print("Dia baru berumur ", umur, " tahun")
 print("dan dia mendapatkan nilai ", nilai)
 print("dan itu sangat hebat sekali")
+
+# contoh penggunaan variabel yang dilarang
+# 1angka = 1  karena dimulai dari angka
+
+# contoh penggunaan variable yang baik
+# nama_lengkap = "jhon lenon"
+# nama_lengkap2 = "jhon lenin"
+
+# contoh inisiasi beberapa variable
+# dalam satu line
+a, b, c = 1, 2, 3
+print("a adalah nomor ", a)  # 1
+print("b adalah nomor ", b)  # 2
+print("c adalah nomor ", c)  # 3
+
+# contoh penggunaan satu value dalam
+# beberapa variabel
+d = e = f = 1
+print("d adalah nomor ", d)  # 1
+print("e adalah nomor ", e)  # 1
+print("f adalah nomor ", f)  # 1
