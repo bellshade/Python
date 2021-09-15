7f0dc81f9605ecb2b6370461601cbf4fb11d81ee
athallahmaajid <athallahmaajid@gmail.com>
2021-09-15 17:49:09 +0700

    add: String

---
+++ Basic/String.py
@@ -0,0 +1,21 @@
+# String adalah teks diawali dengan kutip (" atau ') dan diakhiri dengan kutip juga
+
+# String Dua Kutip (")
+print("Hello")
+
+# Pemisah
+print("=================")
+
+# String Satu Kutip(')
+print('Hello')
+
+# Pemisah
+print("=================")
+
+# String Multiline
+print("""Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.""")
+
+
