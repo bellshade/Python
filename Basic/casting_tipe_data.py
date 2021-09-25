f59bada7358b11fae52719e91dbe64bc15361239
slowy07 <slowy.arfy@gmail.com>
2021-09-25 10:08:41 +0700

    add: tipe casting
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ Basic/casting_tipe_data.py
@@ -0,0 +1,28 @@
+# type casting adalah metode merubah tipe data dari
+# suatu variable atau value yang sudah dideklarasikan
+# contoh dibawah ini
+# kita bisa melihat dengan menggunakan fungsi "type()"
+
+
+# merubah ke bilangan koma ke bilangan bulat
+angka = int(30.12)
+print(type(angka))
+
+# merubah ke bilangan bulat ke bilangan koma
+angka_float = float(20)
+print(type(angka_float))
+
+# merubah dari bilangan bulat ke string
+angka_string = str(30)
+print(type(angka_string))
+
+# contoh lain (tipe data float)
+# float(1)
+# float("2")
+# float(3.14)
+
+
+# contoh lain (tipedata string)
+# string(1)
+# string("2")
+# string(3.14)
