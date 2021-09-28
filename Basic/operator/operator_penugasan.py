2d35b2759e8cae85793700da0d594e2ec196670a
athallahmaajid <athallahmaajid@gmail.com>
2021-09-28 20:34:36 +0700

    add: Operator Penugasan

---
+++ Basic/operator/operator_penugasan.py
@@ -0,0 +1,37 @@
+# Operator penugasan (Assignment Operator) digunakan untuk
+# menetapkan nilai suatu variable
+
+"""
+Operator Penugasan terdiri dari:
+
+    |   Operator    |   Example     |  Sama Dengan  |
+    |     =         |   x = 5       |   x = 5       |
+    |     +=        |   x += 3      |   x = x + 3   |
+    |     -=        |   x -= 3      |   x = x - 3   |
+    |     *=        |   x *= 3      |   x = x * 3   |
+    |     /=        |   x /= 3      |   x = x / 3   |
+    |     %=        |   x %= 3      |   x = x % 3   |
+    |     //=       |   x //= 3     |   x = x // 3  |
+    |     **=       |   x **= 3     |   x = x ** 3  |
+    |     &=        |   x &= 3      |   x = x & 3   |
+    |     |=        |   x |= 3      |   x = x | 3   |
+    |     ^=        |   x ^= 3      |   x = x ^ 3   |
+    |     <<=       |   x <<= 3     |   x = x << 3  |
+    |     >>=       |   x >>= 3     |   x = x >> 3  |
+"""
+
+# contoh:
+
+# variable
+nilai = 5
+
+
+# menetapkan sekaligus menambahkan nilai ke variable nilai
+nilai += 3
+print("nilai += 3 adalah", nilai)
+
+# menetapkan sekaligus mengurangkan nilai ke variable nilai
+nilai -= 3
+print("nilai -= 3 adalah", nilai)
+
+# dan seterusnya
