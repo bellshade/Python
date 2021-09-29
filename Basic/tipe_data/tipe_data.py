9db64f2af974c1fcc54fb4305bb57463eee45f43
khaira nabila <75076265+khairanabila@users.noreply.github.com>
2021-09-29 22:54:52 +0700

    add: tipe_data python (#31)
    * add: tipe_data python
    
    * fix: menghapus titik koma
    
    * fix: menambahkan 'type()'
    
    * fix: update tipe data

---
+++ Basic/tipe_data/tipe_data.py
@@ -0,0 +1,28 @@
+# tipe data pada python adalah
+# tipe text yaitu string
+# tipe angka yaitu int, float, dan complex
+# tipe data majemuk yaitu list, tuple, set, dan range
+# tipe data map yaitu dict
+
+# penggunaan type() sebagai pemeriksa sebuah tipe data
+# pada variabel
+
+# suatu tipe data yang terdapat pada variabel
+# menampilkan contoh tipe data integer
+bilangan_integer = 2
+print(type(bilangan_integer))
+
+# menampilkan tipe data yang merupakan
+# hasil dari kedua value
+operasi = 3 + 3
+print(type(operasi))
+
+# contoh tipe data dari list
+nama_siswa = ["andi", "buzz", "wheezy", "army"]
+print(nama_siswa)
+print(type(nama_siswa))
+
+# contoh tipe data dari dict
+merk_mobil = {"merk": "ferrari", "asal": "italia"}
+print(merk_mobil)
+print(type(merk_mobil))
