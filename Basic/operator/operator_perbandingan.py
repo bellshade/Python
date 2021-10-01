83a8525b1fb98d9b32a3b403fc8b5d48980b11fa
athallahmaajid <athallahmaajid@gmail.com>
2021-10-01 17:51:15 +0700

    fix: lint

--- Basic/operator/operator_perbandingan.py
+++ Basic/operator/operator_perbandingan.py
@@ -9,24 +9,24 @@ b = 3
 
 # == (sama dengan) digunakan untuk membandingkan
 # apakah kedua nilai memiliki nilai yang sama
-print("a == b:", a == b) # hasil yang dikeluarkan adalah False
+print("a == b:", a == b)
 
 # != (tidak sama dengan) digunakan untuk membadingkan
 # apakah kedua nilai tidak memiliki nilai yang sama
-print("a != b:", a != b) # hasil yang dikeluarkan adalah True
+print("a != b:", a != b)
 
 # > (lebih besar dari) digunakan untuk membandingkan
 # apakah nilai kiri lebih besar dari nilai kanan
-print("a > b:", a > b) # hasil yang dikeluarkan adalah True
+print("a > b:", a > b)
 
 # < (lebih kecil dari) digunakan untuk membadingkan
 # apakah nilai kiri lebih kecil dari nilai kanan
-print("a < b:", a < b) # hasil yang dikeluarkan adalah False
+print("a < b:", a < b)
 
 # >= (lebih besar dari atau sama dengan) digunakan untuk membandingkan
 # apakah nilai kiri lebih besar dari atau sama dengan nilai kanan
-print("a >= b:", a >= b) # hasil yang dikeluarkan adalah True
+print("a >= b:", a >= b)
 
 # <= (lebih kecil dari atau sama dengan) digunakan untuk membadingkan
 # apakah nilai kiri lebih kecil dari atau sama dengan nilai kanan
-print("a <= b:", a <= b) # hasil yang dikeluarkan adalah False
+print("a <= b:", a <= b)
