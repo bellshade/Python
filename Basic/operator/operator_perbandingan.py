269931414c9c79c1203f412687016da6579c455e
athallahmaajid <athallahmaajid@gmail.com>
2021-10-01 17:39:58 +0700

    add: Operator Perbandingan

---
+++ Basic/operator/operator_perbandingan.py
@@ -0,0 +1,32 @@
+# Operator Perbandingan (Comparison Operator) digunakan untuk
+# Membadingkan antara dua nilai
+
+# contoh
+
+# Variable
+a = 5
+b = 3
+
+# == (sama dengan) digunakan untuk membandingkan
+# apakah kedua nilai memiliki nilai yang sama
+print("a == b:", a == b) # hasil yang dikeluarkan adalah False
+
+# != (tidak sama dengan) digunakan untuk membadingkan
+# apakah kedua nilai tidak memiliki nilai yang sama
+print("a != b:", a != b) # hasil yang dikeluarkan adalah True
+
+# > (lebih besar dari) digunakan untuk membandingkan
+# apakah nilai kiri lebih besar dari nilai kanan
+print("a > b:", a > b) # hasil yang dikeluarkan adalah True
+
+# < (lebih kecil dari) digunakan untuk membadingkan
+# apakah nilai kiri lebih kecil dari nilai kanan
+print("a < b:", a < b) # hasil yang dikeluarkan adalah False
+
+# >= (lebih besar dari atau sama dengan) digunakan untuk membandingkan
+# apakah nilai kiri lebih besar dari atau sama dengan nilai kanan
+print("a >= b:", a >= b) # hasil yang dikeluarkan adalah True
+
+# <= (lebih kecil dari atau sama dengan) digunakan untuk membadingkan
+# apakah nilai kiri lebih kecil dari atau sama dengan nilai kanan
+print("a <= b:", a <= b) # hasil yang dikeluarkan adalah False
