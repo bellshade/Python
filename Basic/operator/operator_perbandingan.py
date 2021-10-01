261a728a4f6525a486d6a19984a93de726e47bab
athallahmaajid <athallahmaajid@gmail.com>
2021-10-01 21:49:02 +0700

    fix: expected output and add readme

--- Basic/operator/operator_perbandingan.py
+++ Basic/operator/operator_perbandingan.py
@@ -9,24 +9,25 @@ b = 3
 
 # == (sama dengan) digunakan untuk membandingkan
 # apakah kedua nilai memiliki nilai yang sama
-print("a == b:", a == b)
+print("a == b:", a == b) # False
 
 # != (tidak sama dengan) digunakan untuk membadingkan
 # apakah kedua nilai tidak memiliki nilai yang sama
-print("a != b:", a != b)
+print("a != b:", a != b) # True
 
 # > (lebih besar dari) digunakan untuk membandingkan
 # apakah nilai kiri lebih besar dari nilai kanan
-print("a > b:", a > b)
+
+print("a > b:", a > b) # True
 
 # < (lebih kecil dari) digunakan untuk membadingkan
 # apakah nilai kiri lebih kecil dari nilai kanan
-print("a < b:", a < b)
+print("a < b:", a < b) # False
 
 # >= (lebih besar dari atau sama dengan) digunakan untuk membandingkan
 # apakah nilai kiri lebih besar dari atau sama dengan nilai kanan
-print("a >= b:", a >= b)
+print("a >= b:", a >= b) # True
 
 # <= (lebih kecil dari atau sama dengan) digunakan untuk membadingkan
 # apakah nilai kiri lebih kecil dari atau sama dengan nilai kanan
-print("a <= b:", a <= b)
+print("a <= b:", a <= b) # False
