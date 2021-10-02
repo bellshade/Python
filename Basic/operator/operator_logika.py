548119d1b7ae6ad95b4967cc0c36edeebe4a6345
athallahmaajid <athallahmaajid@gmail.com>
2021-10-02 19:27:58 +0700

    add: Operator Logika

---
+++ Basic/operator/operator_logika.py
@@ -0,0 +1,28 @@
+"""
+operator logika (Logical Operator) digunakan untuk
+menggabungkan pernyataan kondisional
+
+bingung?
+mari kita buat contohnya
+"""
+
+# variabel
+x = 5
+
+# operator "and" digunakan untuk mengecek
+# apakah kedua pernyataan yang diberikan bernilai True
+
+print("x < 6 and x > 1:", x < 6 and x > 1)  # True
+print("x > 6 and x > 1:", x > 6 and x > 1)  # False
+
+# operator "or" digunakan untuk mengecek
+# apakah salah satu dari dua pernyataan bernilai True
+
+print("x > 6 or x > 1:", x > 6 or x > 1)  # True
+print("x > 6 or x < 1:", x > 6 or x < 1)  # False
+
+# operator "not" digunakan untuk mengecek
+# apakah nilai tersebut bernilai False
+
+print(not(x < 6))  # False
+print(not(x > 10))  # True
