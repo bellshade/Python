2578b0fc19fd4d5541a78ac6c010b5c6d9264d3e
KelvinMulyawan <kelvinmulyawan2000@gmail.com>
2021-10-03 12:19:36 +0700

    Update for_loop.py

--- Basic/looping/for_loop.py
+++ Basic/looping/for_loop.py
@@ -1,27 +1,28 @@
 # membuat list
-angka = [1,2,3,4,5]
+angka = [1, 2, 3, 4, 5]
 
 
 # basic for loop
 for item in angka:
     print(item)
-    
+
 # for loop and expression
 for item in angka:
-    print(item*2)
-    
+    print(item * 2)
+
 for item in angka:
     print(item)
     if item == 3:
         break
-    
+
 for item in angka:
     if item == 3:
         continue
     print(item)
-    
-for item in angka:
+
+daftar = []
+for item in daftar:
     print(item)
     break
 else:
-    print("Selesai !")
+    print("Daftar Kosong")
