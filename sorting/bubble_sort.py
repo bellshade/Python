c985a10c148fffd75091b13601cd68e2971c0964
Vishesh-dd4723 <bindal.vishesh@gmail.com>
2021-10-04 15:57:26 +0530

    Added Insertion Sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,14 +1,11 @@
-#Waktu proses adalah O(n²).
-#Tidak diperlukan ruang baru.
+# Waktu proses adalah O(n²).
+# Tidak diperlukan ruang baru.
 
 def Bubble_Sort(array, length):
-    #array: array angka yang tidak diurutkan
-    #length: panjang larik
+    # array: array angka yang tidak diurutkan
+    # length: panjang larik
     for i in range(0, length):
         for j in range(0, length - i - 1):
-            #Jika elemen sebelumnya lebih besar dari pertukaran itu.
+            # Jika elemen sebelumnya lebih besar dari pertukaran itu.
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
-
-array = list(map(int, input().split()))
-Bubble_Sort(array, len(array))
