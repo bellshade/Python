2a45afd2211a3db36ac1a2e6078ac952e64ce639
athallahmaajid <athallahmaajid@gmail.com>
2021-10-04 09:54:56 +0700

    fix: pylintrc

--- Basic/operator/operator_logika.py
+++ Basic/operator/operator_logika.py
@@ -1,3 +1,4 @@
+
 """
 operator logika (Logical Operator) digunakan untuk
 menggabungkan pernyataan kondisional
@@ -7,22 +8,25 @@ mari kita buat contohnya
 """
 
 # variabel
-x = 5
+X = 5
 
 # operator "and" digunakan untuk mengecek
 # apakah kedua pernyataan yang diberikan bernilai True
 
-print("x < 6 and x > 1:", x < 6 and x > 1)  # True
-print("x > 6 and x > 1:", x > 6 and x > 1)  # False
+print("X < 6 and X > 1:", X < 6 and X > 1)  # True
+print("X > 6 and X > 1:", X > 6 and X > 1)  # False
 
 # operator "or" digunakan untuk mengecek
 # apakah salah satu dari dua pernyataan bernilai True
 
-print("x > 6 or x > 1:", x > 6 or x > 1)  # True
-print("x > 6 or x < 1:", x > 6 or x < 1)  # False
+print("X > 6 or X > 1:", X > 6 or X > 1)  # True
+print("X > 6 or X < 1:", X > 6 or X < 1)  # False
 
 # operator "not" digunakan untuk mengecek
 # apakah nilai tersebut bernilai False
 
-print(not x < 6)  # False
-print(not x > 10)  # True
+X_UNDER_6 = X < 6
+X_ABOVE_10 = X > 10
+
+print(not X_UNDER_6)  # False
+print(not X_ABOVE_10)  # True
