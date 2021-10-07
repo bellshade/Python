9b5bcc1ed54988d46d0e1b389474fa6f08a34fd0
dikihmd01 <dikihamdani.id@gmail.com>
2021-10-07 15:07:39 +0700

    Update comments in operator_identitas.py

--- Basic/operator/operator_identitas.py
+++ Basic/operator/operator_identitas.py
@@ -1,8 +1,9 @@
 """
-Operator Identitas adalah operator yang digunakan untuk cek 
-apakah nilai dari suatu varibel berada pada memori yang sama atau tidak
+Operator Identitas adalah operator yang digunakan
+untuk cek apakah nilai dari suatu varibel berada
+pada memori yang sama atau tidak
 
-contoh:
+contoh
 """
 
 # variabel
@@ -12,23 +13,27 @@ z = "Python"
 list_ular1 = ["Python", "Cobra", "Sanca"]
 list_ular2 = ["Python", "Cobra", "Sanca"]
 
+# cek apakah nilai dari variabel
+# x dan y ada pada memori yang sama
+print("x is y = ", x is y)    # False
 
-# cek apakah nilai dari variabel x dan y ada pada memori yang sama 
-print("x is y = ", x is y) # False
+# cek apakah nilai dari variabel
+# x dan z ada pada memori yang sama
+print("x is z = ", x is z)    # True
 
-# cek apakah nilai dari variabel x dan z ada pada memori yang sama 
-print("x is z = ", x is z) # True
-
-# cek apakah nilai dari variabel list_ular1 dan list_ular2 ada pada memori yang sama
-print("list_ular1 is list_ular2 = ", list_ular1 is list_ular2) # False
+# cek apakah nilai dari variabel
+# list_ular1 dan list_ular2 ada pada memori yang sama
+print("list_ular1 is list_ular2 = ", list_ular1 is list_ular2)    # False
 print("\n")
 
-# cek apakah nilai dari variabel list_ular1 dan list_ular2 tidak pada memori yang sama
-print("x is not y = ", x is not y) # True
+# cek apakah nilai dari variabel
+# list_ular1 dan list_ular2 tidak pada memori yang sama
+print("x is not y = ", x is not y)    # True
 
-# cek apakah nilai dari variabel list_ular1 dan list_ular2 tidak pada memori yang sama
-print("x is not z = ", x is not z) # False
+# cek apakah nilai dari variabel
+# list_ular1 dan list_ular2 tidak pada memori yang sama
+print("x is not z = ", x is not z)    # False
 
-# cek apakah nilai dari variabel list_ular1 dan list_ular2 tidak pada memori yang sama 
-print("list_ular1 is list_ular2 = ", list_ular1 is not list_ular2) # True
-
+# cek apakah nilai dari variabel
+# list_ular1 dan list_ular2 tidak pada memori yang sama
+print("list_ular1 is list_ular2 = ", list_ular1 is not list_ular2)    # True
