8051b2d9928353cfac69aabe899e878b75c23f61
fadhilc97 <fadhilc97@gmail.com>
2021-10-07 09:55:32 +0800

    fix: menambahkan komentar 'type: ignore' agar lolos pengecekan mypy

--- Basic/exception/exception.py
+++ Basic/exception/exception.py
@@ -3,7 +3,7 @@ try:
     # jalankan kode berikut jika berhasil
     # bilangan yang dibagi dengan nol merupakan kesalahan
     # maka tangani di dalam 'except'
-    hasil = 5 / 0
+    hasil = 5 / 0  # type: ignore
     print('Hasil pembagian adalah =', hasil)
 except ZeroDivisionError:
     # tangani kesalahan di sini
@@ -13,7 +13,7 @@ except ZeroDivisionError:
 # menangani lebih dari satu eksepsi
 # Cara 1. menggunakan lebih dari satu blok 'except'
 try:
-    hasil = 3 / "1"
+    hasil = 3 / "1"  # type: ignore
     print('Hasil pembagian adalah =', hasil)
 except ZeroDivisionError:
     print("Bilangan tidak dapat dibagi dengan nol")
@@ -22,7 +22,7 @@ except TypeError:
 
 # Cara 2. menuliskannya di dalam tuple
 try:
-    hasil = 3 / "1"
+    hasil = 3 / "1"  # type: ignore
     print('Hasil pembagian adalah =', hasil)
 except (ZeroDivisionError, TypeError):
     print("Terdapat kesalahan ketika melakukan pembagian")
@@ -31,7 +31,7 @@ except (ZeroDivisionError, TypeError):
 # mencetak pesan kesalahan/eksepsi bawaan python
 # gunakan keyword 'as' untuk memberi nama alias eksepsi
 try:
-    hasil = 5 / 0
+    hasil = 5 / 0  # type: ignore
     print("Hasil pembagian adalah =", hasil)
 except ZeroDivisionError as err:
     print(err)  # division by zero
@@ -39,7 +39,7 @@ except TypeError as err:
     print(err)  # unsupported operand type(s) for /: 'int' and 'str'
 
 try:
-    hasil = 3 / "1"
+    hasil = 3 / "1"  # type: ignore
     print('Hasil pembagian adalah =', hasil)
 except (ZeroDivisionError, TypeError) as err:
     # cetak pesan error tergantung jenis error yang ditangkap
