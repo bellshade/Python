4c98fb4b233961de762f62b6d3ba82c37e2ea011
MarvinYoung <marvin.young2108@gmail.com>
2021-10-08 19:31:15 +0700

    fix:Memperbaiki error(7)

--- other/isPrime.py
+++ other/isPrime.py
@@ -1,13 +1,15 @@
-#Bilangan Prima adalah Bilangan yang habis dibagi oleh bilangan itu sendiri dan bilangan 1
+# Bilangan Prima adalah Bilangan yang habis dibagi oleh bilangan itu sendiri dan bilangan 1
 def isPrime(num):
-    #Mengecek apakah num merupakan bilangan negatif dan dibawah 2
-    if(num<2):return ('Bukan Bilangan Prima')
+    # Mengecek apakah num merupakan bilangan negatif dan dibawah 2
+    if num < 2:
+        return "Bukan Bilangan Prima"
     # Perulangan disini dimulai dari 2 dikarenakan kita bilangan prima dimulai dari 2 dan seterusnya
-    for i in range(2,num,1):
-        #Jika num habis dibagi oleh i maka bilangan tersebut bukan Bilangan prima
-        if(num%i==0):
-            return ('Bukan Bilangan Prima')
-    return ('Bilangan Prima')
+    for i in range(2, num, 1):
+        # Jika num habis dibagi oleh i maka bilangan tersebut bukan Bilangan prima
+        if num % i == 0:
+            return "Bukan Bilangan Prima"
+    return "Bilangan Prima"
 
-print(isPrime(20)) #output "Bukan Bilangan Prima"
-print(isPrime(5)) #output "Bilangan Prima"
+
+print(isPrime(20))  # output "Bukan Bilangan Prima"
+print(isPrime(5))  # output "Bilangan Prima"
