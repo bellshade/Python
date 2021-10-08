572bc6b692409e33d2095f823e4ff0fa3d1ad117
MarvinYoung <marvin.young2108@gmail.com>
2021-10-08 17:23:06 +0700

    fix: Memperbaiki error(2)

--- other/isPrime.py
+++ other/isPrime.py
@@ -2,17 +2,12 @@
 def isPrime(num):
     #Mengecek apakah num merupakan bilangan negatif dan dibawah 2
     if(num<2):return ('Bukan Bilangan Prima')
-    """
-    Perulangan disini dimulai dari 2 dikarenakan kita bilangan prima dimulai dari 2 dan seterusnya
-    Setiap perulangan i akan di tambah 1
-    Batas perulangan sampai pada num
-    """
+    # Perulangan disini dimulai dari 2 dikarenakan kita bilangan prima dimulai dari 2 dan seterusnya
     for i in range(2,num,1):
         #Jika num habis dibagi oleh i maka bilangan tersebut bukan Bilangan prima
         if(num%i==0):
-            return 'Bukan Bilangan Prima'
-        
+            return 'Bukan Bilangan Prima'      
     return 'Bilangan Prima'
-    
+
 print(isPrime(20)) #output "Bukan Bilangan Prima"
 print(isPrime(5)) #output "Bilangan Prima"
