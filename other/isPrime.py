e90aec5081e63981970f9fc3ea726792ed269ad9
MarvinYoung <marvin.young2108@gmail.com>
2021-10-08 20:10:18 +0700

    fix:Memperbaiki error(8)

--- other/isPrime.py
+++ other/isPrime.py
@@ -1,9 +1,11 @@
-# Bilangan Prima adalah Bilangan yang habis dibagi oleh bilangan itu sendiri dan bilangan 1
+# Bilangan Prima adalah Bilangan yang habis dibagi
+# oleh bilangan itu sendiri dan bilangan 1
 def isPrime(num):
     # Mengecek apakah num merupakan bilangan negatif dan dibawah 2
     if num < 2:
         return "Bukan Bilangan Prima"
-    # Perulangan disini dimulai dari 2 dikarenakan kita bilangan prima dimulai dari 2 dan seterusnya
+    # Perulangan disini dimulai dari 2 dikarenakan 
+    # bilangan prima dimulai dari 2 dan seterusnya
     for i in range(2, num, 1):
         # Jika num habis dibagi oleh i maka bilangan tersebut bukan Bilangan prima
         if num % i == 0:
