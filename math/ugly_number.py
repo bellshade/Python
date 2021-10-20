78946b5d1044e940799ba82669d3a5ce1d8069c7
slowy07 <slowy.arfy@gmail.com>
2021-10-20 19:12:05 +0700

    feat: menambahkan bilangan aneh
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ math/ugly_number.py
@@ -0,0 +1,35 @@
+# bilangan aneh adalah bilangan
+# yang faktor primanya hanya 2, 3, 5
+# urutannya
+# 1, 2, 3, 4 ,5 ,6, 8, 9, 10, 12, 15, ...
+# menunjukkan angka aneh pertama.
+# dengan perjanjian
+# 1 disertakan. deiberikan bilangan bilan n,
+# kita harus menemukan bilangan aneh ke-n
+
+
+def ugly_number(n: int) -> int:
+    ugly_num = [1]
+    i2, i3, i5 = 0, 0, 0
+    next_2 = ugly_num[i2] * 2
+    next_3 = ugly_num[i3] * 3
+    next_5 = ugly_num[i5] * 5
+
+    for i in range(1, n):
+        next_num = min(next_2, next_3, next_5)
+        ugly_num.append(next_num)
+        if next_num == next_2:
+            i2 += 1
+            next_2 = ugly_num[i2] * 2
+        if next_num == next_3:
+            i3 += 1
+            next_3 = ugly_num[i3] * 3
+
+    return ugly_num[-1]
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
+    print(f"{ugly_number(200)}")
