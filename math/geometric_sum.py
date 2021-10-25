7aeb68bdedc993db9243ad30059665b1ec6ae9dd
arfy slowy <slowy.arfy@gmail.com>
2021-10-26 00:19:18 +0700

    fix: update doctest dan typehint

--- math/geometric_sum.py
+++ math/geometric_sum.py
@@ -1,18 +1,22 @@
-def geoSum(suku_pertama, rasio, jumlah_deret):
+from __future__ import annotations
+
+
+def geoSum(suku_pertama: int, rasio: int, jumlah_deret: int) -> float:
     """
     Menghitung deret geometri berhingga dari n buah suku
     dengan suku pertama a dan rasio r. Rumus umum berlaku
     jika r tidak sama dengan 1 dan a tidak sama dengan 0
-
-    Rumus : a * ((1 -  pow(r, n)) / (1 - r))
-
+    Rumus = a * ((1 -  pow(r, n)) / (1 - r))
     Mengembalikan nilai perhitungan rumus
 
-    Valid input :
-    >>> geoSum(0, 0.5, 3)
-    21.0
+    >>> geoSum(2, 2, 3)
+    14.0
 
-    Invalid input :
+    >>> geoSum(0, 0.5, 3)
+    Traceback (most recent call last):
+    ...
+    ValueError: Suku pertama tidak boleh 0
+
     >>> geoSum(3, 1, 5)
     Traceback (most recent call last):
     ...
@@ -31,10 +35,15 @@ def geoSum(suku_pertama, rasio, jumlah_deret):
     return result
 
 
-# menampilkan 93.0
-hasil1 = geoSum(3, 2, 5)
-print(hasil1)
+if __name__ == "__main__":
+    import doctest
 
-# me-raise exception
-hasil2 = geoSum(3, 1, 5)
-print(hasil2)
+    doctest.testmod()
+
+    # menampilkan 93.0
+    # hasil1 = geoSum(3, 2, 5)
+    # print(hasil1)
+
+    # me-raise exception
+    # hasil2 = geoSum(3, 1, 5)
+    # print(hasil2)
