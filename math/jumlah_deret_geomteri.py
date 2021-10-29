fdb0d0aa2c4b4a1014cf89b5140725666ae2d042
slowy07 <slowy.arfy@gmail.com>
2021-10-29 23:48:57 +0700

    feat: menambahkan deret geomtri
    close #118
    
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ math/jumlah_deret_geomteri.py
@@ -0,0 +1,26 @@
+# informasi tentang geometri progression
+# https://en.wikipedia.org/wiki/Geometric_progression
+
+
+def jumlah_deret_geometri(utama: int, rasio_umum: int, jumlah: int) -> int:
+    """
+    Menghitung jumlah deret geometri
+    >>> jumlah_deret_geometri(1, 2, 10)
+    1023.0
+    >>> jumlah_deret_geometri(1, 10, 5)
+    11111.0
+    >>> jumlah_deret_geometri(0, 2, 10)
+    0.0
+    """
+    if rasio_umum == 1:
+        # Rumus jumlah jika rasio umum adalah 1
+        return jumlah * utama
+
+    # Rumus untuk mencari jumlah n suku dari Progresi Geometris
+    return (utama / (1 - rasio_umum)) * (1 - rasio_umum ** jumlah)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
