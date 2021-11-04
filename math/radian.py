3a5bce66f28803bfd40053d227838c91f2264a7f
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2021-11-04 17:45:48 +0700

    feat: tambah script untuk menghitung radian

--- math/radian.py
+++ math/radian.py
@@ -1,41 +1,47 @@
 import math
 
-"""
-Fungsi-fungsi di bawah mengonversi masukan dalam
-satuan radian, derajat, dan gradian. Fungsi yang dibuat
-adalah fungsi untuk mengonversi :
-1. radian ke derajat (radian_to_degree())
-2. derajat ke radian (degree_to_radian())
-3. radian ke gradian (radian_to_gradian())
-4. gradian ke radian (gradian_to_radian())
-
->>> radian_to_degree(5)
-286.4788975654116
-
->>> degree_to_radian(60)
-1.0471975511965976
-
->>> radian_to_gradian(3)
-190.9859317102744
-
->>> gradian_to_radian(53)
-0.8325220532012952
-"""
-
 
 def radian_to_degree(radian: float) -> float:
+    """
+    Fungsi ini digunakan untuk mengonversi radian ke derajat.
+    Rumus : radian * (180 derajat / pi)
+
+    >>> radian_to_degree(5)
+    286.4788975654116
+    """
     return radian * (180 / math.pi)
 
 
 def degree_to_radian(degree: float) -> float:
+    """
+    Fungsi ini digunakan untuk mengonversi derajat ke radian.
+    Rumus : derajat * (pi / 180 derajat)
+
+    >>> degree_to_radian(60)
+    1.0471975511965976
+    """
     return degree * (math.pi / 180)
 
 
 def radian_to_gradian(radian: float) -> float:
+    """
+    Fungsi ini digunakan untuk mengonversi radian ke gradian.
+    Rumus : radian * (200 gradian / pi)
+
+    >>> radian_to_gradian(3)
+    190.9859317102744
+    """
     return radian * (200 / math.pi)
 
 
 def gradian_to_radian(gradian: float) -> float:
+    """
+    Fungsi ini digunakan untuk mengonversi radian ke gradian.
+    Rumus : gradian * (180 / (200 gradian)
+
+    >>> gradian_to_radian(53)
+    0.8325220532012952
+    """
     return gradian * (math.pi / 200)
 
 
