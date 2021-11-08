6a74c130bdb0488d055709473d53a6b7f7ef5e7a
Kelvin Mulyawan <87604509+KelvinMulyawan@users.noreply.github.com>
2021-11-08 20:35:52 +0700

    fix:bahasa indonesia

--- Basic/list_comprehension.py
+++ Basic/list_comprehension.py
@@ -4,25 +4,25 @@ original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 print("Original : {}".format(original))
 
-# Iterable
+# Implementasi List Comprehension dengan Iterable
 
 original_dua = [value for value in range(5, 11)]
 
 print("Original Range : {}".format(original_dua))
 
-# Exponent List Comprehension
+# Implementasi List Comprehension dengan Pemangkatan (Exponential)
 
 exp_list = [item ** 2 for item in original]
 
 print("Exponent List : {}".format(exp_list))
 
-# If List Comprehension
+# Implementasi List Comprehension dengan If Else
 
 genap = [item for item in original if item % 2 == 0]
 
 print("Genap : {}".format(genap))
 
-# Expression List Comprehension
+# Implementasi List Comprehension dengan Expression
 
 elemen = ["Api", "Air", "Tanah", "Udara"]
 
