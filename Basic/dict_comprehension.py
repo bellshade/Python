e7baa715f5586a3db8111250c0b6d29c08ba6cae
Kelvin Mulyawan <87604509+KelvinMulyawan@users.noreply.github.com>
2021-11-08 20:38:33 +0700

    Update and rename other/dict_comprehension.py to Basic/dict_comprehension.py

--- Basic/dict_comprehension.py
+++ Basic/dict_comprehension.py
@@ -1,19 +1,20 @@
+# Membuat Dictionary
 angka = dict()
 
 # Basic Dict Comprehension
 angka = {nilai: nilai * 2 for nilai in range(1, 6)}
 print("Angka : ", angka)
 
-# More on Dict Comprehension
+# Contoh lainnya pada Dict Comprehension
 pangkat = {key * 2: value ** 2 for (key, value) in angka.items()}
 print("Pangkat Dua: ", pangkat)
 
-# If Dict Comprehension
+# Implementasi Dict Comprehension Dengan If Else
 ten_nums = {value: value * 2 for value in range(1, 11)}
 genap_only = {key: value for (key, value) in ten_nums.items() if key % 2 == 0}
 print("Genap Only : ", genap_only)
 
-# If Else Dict Comprehension
+# Implementasi Dict Comprehension Dengan If Else
 age_name = {"Ronaldo": 37, "Messi": 34, "Greenwood": 20, "Sancho": 21}
 status_umur = {
     nama: ("Tua" if umur > 30 else "Muda") for (nama, umur) in age_name.items()
