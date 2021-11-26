b3f0eedef05473347e00f4b686501592ee30c96e
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-11-26 20:42:52 +0700

    feat: menambahkan docstring dan doctest

--- other/is_palindrom.py
+++ other/is_palindrom.py
@@ -1,12 +1,36 @@
 def is_palindrom(x: str) -> bool:
+    """
+    Palindrom merupakan kata yang dapat dibaca dengan sama baik dari depan
+    maupun belakang.
+
+    Fungsi ini mengecek apakah `x` merupakan sebuah palindrom. Fungsi ini
+    akan mengembalikan `True` jika `x` merupakan palindrom, dan `False`
+    jika tidak.
+
+    >>> is_palindrom("katak")
+    True
+    >>> is_palindrom("label")
+    False
+    >>> is_palindrom("Kasur ini rusak")
+    True
+    """
     normalized = x.lower().replace(" ", "")
     
     return normalized == normalized[::-1]
 
 def main(args=None):
+    import doctest
+
+    doctest.testmod()
+
+    # basic tests
     print(is_palindrom("katak"))            # output "True"
     print(is_palindrom("label"))            # output "False"
     print(is_palindrom("Kasur ini rusak"))  # output "True"
+
+    # custom tests
+    # aktifkan baris berikutnya untuk menjalankan custom test
+    # print(is_palindrom(input('Masukkan kata: ')))
 
 
 if __name__ == '__main__':
