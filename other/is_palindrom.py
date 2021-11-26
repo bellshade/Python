d5dc2c614b64ab5162def3b0ce9b4a1346b69390
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-11-26 20:33:34 +0700

    fix: rename `Palindrom.py` to `is_palindrom.py`

--- other/is_palindrom.py
+++ other/is_palindrom.py
@@ -1,9 +1,8 @@
 # Palindrom adalah kata yang dapat
 # dibaca dari depan maupun belakang
-def palindrom(x):
+def is_palindrom(x):
     # Pengecekan apakah huruf didepan dan belakang itu sama
     return x == x[::-1]
 
-
-print(palindrom("katak"))  # output "True/Palindrom"
-print(palindrom("label"))  # output "False/Bukan Palindrom"
+print(is_palindrom("katak"))  # output "True/Palindrom"
+print(is_palindrom("label"))  # output "False/Bukan Palindrom"
