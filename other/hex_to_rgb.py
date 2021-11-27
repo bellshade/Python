d3cf34aa5ee9ec2e12839e63f1006dd333beffb0
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-11-27 13:35:35 +0700

    feat: menambahkan docstring dan doctest

--- other/hex_to_rgb.py
+++ other/hex_to_rgb.py
@@ -1,4 +1,22 @@
 def hex_to_rgb(hex_color):
+    """
+    Fungsi ini menerima inputan berupa string hexadecimal
+    dan mengembalikan hasilnya dalam bentuk tuple (r, g, b).
+
+    Contoh:
+    >>> hex_to_rgb('#000000')
+    (0, 0, 0)
+    >>> hex_to_rgb('#FFFFFF')
+    (255, 255, 255)
+    >>> hex_to_rgb('#0ef')
+    (0, 238, 255)
+    >>> hex_to_rgb('800040')
+    (128, 0, 64)
+    >>> hex_to_rgb('#aabbccdd')
+    Traceback (most recent call last):
+        ...
+    ValueError: Hexadecimal tidak valid
+    """
     hex_string = hex_color.lstrip("#")
 
     if len(hex_string) == 3:
@@ -11,11 +29,20 @@ def hex_to_rgb(hex_color):
 
 
 def main(args=None):
+    import doctest
+
+    doctest.testmod()
+
+    # basic tests
     print(hex_to_rgb('#000000'))    # (0, 0, 0)
     print(hex_to_rgb('#FFFFFF'))    # (255, 255, 255)
     print(hex_to_rgb('#0ef'))       # (0, 238, 255)
     print(hex_to_rgb('800040'))     # (128, 0, 64)
     # print(hex_to_rgb('#aabbccdd'))  # ValueError: Hexadecimal tidak valid
+
+    # custom test
+    # aktifkan baris berikutnya untuk menjalankan custom test
+    # print(hex_to_rgb(input("Masukkan warna hex:")))
 
 
 if __name__ == '__main__':
