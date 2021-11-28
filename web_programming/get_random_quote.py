10e5c76aee92c7e2c442a56d0a9086fafd44f6c4
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-11-29 01:54:08 +0700

    add: menambahkan `get_random_quote.py` (#175)
    * add: menambahkan `get_random_quote.py`
    
    Fungsi sederhana yang memanggil API Quotable
    
    * feat: menambah interaktivitas
    
    Menambahkan interaktivitas saat dijalankan sebagai script
    
    * feat: menambahkan comment penjelasan fungsi

---
+++ web_programming/get_random_quote.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+
+def get_random_quote():
+    # Fungsi ini akan mengambil quote secara random dari API Quotable
+    # dan mengembalikan quote yang diambil.
+    #
+    # Link repository API Quotable:
+    # https://github.com/lukePeavey/quotable
+    url = "https://api.quotable.io/random"
+    r = requests.get(url)
+    quote = json.loads(r.text)
+    return quote
+
+
+def main():
+    import os
+
+    quote = get_random_quote()
+
+    # clear screen
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"\n\n{quote['content']} - {quote['author']}")
+
+    if input("\n\nKetik 'y' untuk coba lagi: ") == 'y':
+        main()
+
+
+if __name__ == '__main__':
+    main()
