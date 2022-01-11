599e4cffec48a2cafe39e6fb059bc305fae014fe
slowy07 <slowy.arfy@gmail.com>
2022-01-11 19:53:01 +0700

    feat: menambahkan kode contoh penggunaan API
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ web_programming/emisi_co2.py
@@ -0,0 +1,25 @@
+# contoh sederhana emisi CO2 dari sebuah negara inggris
+# yang menggunakan API dari https://api.carbonintensity.org.uk
+
+from datetime import date
+
+import requests
+
+BASE_URL = "https://api.carbonintensity.org.uk/intensity"
+
+
+# emisi dalam setengah jam terakhir
+def emisi_stngh_jam() -> str:
+    setengah_jam = requests.get(BASE_URL).json()["data"][0]
+    return setengah_jam["intensity"]["actual"]
+
+
+# emisi dalam waktu spesifik
+def emisi(start, end) -> list:
+    return requests.get(f"{BASE_URL}/{start}/{end}").json()["data"]
+
+
+if __name__ == "__main__":
+    for entry in emisi(start=date(2020, 10, 1), end=date(2020, 10, 3)):
+        print("dari {from} ke {to}: {intensity[actual]}".format(**entry))
+    print(f"{emisi_stngh_jam() = }")
