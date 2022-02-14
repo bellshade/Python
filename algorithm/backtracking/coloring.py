a8419618be67d2eb023c255fb7a034e626845b0f
bellshadebot <bellshade07@gmail.com>
2022-02-14 12:17:07 +0000

    style: format otomatis dengan black dan isort

--- algorithm/backtracking/coloring.py
+++ algorithm/backtracking/coloring.py
@@ -4,6 +4,8 @@
 # Wikipedia: https://en.wikipedia.org/wiki/Graph_coloring
 
 from typing import List
+
+
 def coloring(neighbours: List[int], colored_vertices: List[int], color: int) -> bool:
     """
     Untuk setiap pemeriksaan apakah kendala pewarna terpenuhi
