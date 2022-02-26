b00bf7c62f6b4d3e10c7ab9bd4beb0446c697c55
bellshadebot <bellshade07@gmail.com>
2022-02-26 11:34:35 +0000

    style: format otomatis dengan black dan isort

--- algorithm/arithmetic_analysis/jacobi_iteration_method.py
+++ algorithm/arithmetic_analysis/jacobi_iteration_method.py
@@ -10,6 +10,7 @@ from __future__ import annotations
 
 import numpy as np
 
+
 # metode untuk mencari solusi sistem persamaan linear
 def jacobi_iteration_method(
     coefficient_matrix: np.ndarray,
