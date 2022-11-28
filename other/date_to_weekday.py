d46dd0f45cfcdaf30bc6a44a7325b9dfdd94a208
arfy slowy <slowy.arfy@gmail.com>
2022-11-29 06:47:25 +0700

    fix: memperbaiki percent-format (CWE-685) (#219)
    * fix: memperbaiki percent-format (CWE-685)
    
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>
    
    * fix: memperbaiki false positive
    
    variabel yang bersifat ambigu catch dengan kode E741
    
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- other/date_to_weekday.py
+++ other/date_to_weekday.py
@@ -16,9 +16,8 @@ def date_to_weekday(inp_date: str) -> str:
     day, month, year = (int(x) for x in inp_date.split("/"))
     if year % 100 == 0:
         year = "00"
-    new_base_date: str = (
-        f"{day}/{month}/{year%100} 0:0:0"  # lgtm [py/percent-format/wrong-arguments]
-    )
+    year = year % 100
+    new_base_date: str = f"{day}/{month}/{year} 0:0:0"
     date_time_obj: datetime.date = datetime.strptime(new_base_date, "%d/%m/%y %H:%M:%S")
     out_put_day: int = date_time_obj.weekday()
     return day_name[out_put_day]
@@ -26,5 +25,7 @@ def date_to_weekday(inp_date: str) -> str:
 
 if __name__ == "__main__":
     import doctest
+
+    print(date_to_weekday("29/11/2022"))
 
     doctest.testmod()
