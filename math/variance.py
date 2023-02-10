a687d076c9d93b2cb33d6058340acfd6cc8d1e09
kayabaakihiko13 <iqbalramad75@gmail.com>
2023-02-10 22:51:06 +0700

    penambahan fungsi variance

---
+++ math/variance.py
@@ -0,0 +1,26 @@
+def rata_rata(data: list[int]) -> float:
+    _sum=0
+    panjang_data=0 #
+    for number in (data):
+        panjang_data+=1
+        _sum+=number
+    result=_sum/panjang_data
+    return round(result,2)
+def variance(data:list[int]):
+    '''
+    variansi atau dalam bahasa inggris disebut juga disebut variance variance.
+    variansi ini berfungsi untuk mengetahuo keragaman data baik agar mengetahui sifat-sifat serta seberan data
+    >>> variance([10,20,20])
+        33.3333335
+    '''
+    n=len(data)
+    _sum=0
+    for value in data:
+        hitung=(value-rata_rata(data))**2
+        _sum+=hitung
+    result=_sum/(n-1)
+    return round(result,2)
+print(rata_rata([20,10,10]))
+if __name__=="__main__":
+    import doctest
+    doctest.testmod(verbose=True)
