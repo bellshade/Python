788b8338c4a2eaf3224694e8e6f64f48e06b4b0e
kayabaakihiko13 <iqbalramad75@gmail.com>
2023-03-13 21:33:30 +0700

    feat:Menambahkan algoritma mean,media,modus

---
+++ implementation/statistic/median.py
@@ -0,0 +1,42 @@
+def median(arr : list) -> int | float:
+    """
+    median adalah nilai yang dapat membagi data menjadi dua bagian yang sama.
+    dengan kata lain bagaimana cara
+
+    **Params**
+
+    -----
+    arr: tipe data ini adalah
+
+    **Return**
+
+    -----
+    value : value ini tergantung pada ganjil atau genapnya jumlah panjang data
+
+    **Contoh**
+
+    ---
+    >>> data=[1,2,3,4,5,6]
+    >>> median(data)
+    3.5
+    >>> data=[1,1,2,2,2,3,3]
+    >>> median(data)
+    2
+    """
+
+    " mengurutkan dari terkecil ke besar "
+    arr.sort()
+    panjang_data = len(arr)
+
+    if panjang_data % 2 == 0:
+        index = panjang_data - 1
+        hasil = (arr[index // 2] + arr[(index // 2) + 1]) / 2
+    else:
+        index = panjang_data - 1
+        hasil = arr[(index + 1) // 2]
+    return hasil
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
---
+++ implementation/statistic/modus.py
@@ -0,0 +1,39 @@
+def mode(arr: list[int | float]) -> int | float:
+    """
+    mode atau dalam bahasa indonesia adalah modus
+    modus dalam statiska merupaka nilai yang sering muncul
+    dalam suatu kumpulan data.
+
+    **Parameter**
+
+    ---
+    :param arr: parameter ini bertipe data list
+
+    **Return**
+    :param return: output ini muncul sebagai sesuai dengan item yang sering terjadi
+
+    **Contoh**
+    >>> data=[1,1,2,2,2,3,3]
+    >>> mode(data)
+    2
+    """
+    count = []
+    for value in arr:
+        "kita hitung jumlah frekuensi pada setiap value di array tersebut  "
+        count.append(arr.count(value))
+    combine = dict(zip(arr , count))
+    memo = [a for a , b in combine.items() if b == max(count)]
+    min_value = memo[0]
+    result = None
+    for value in memo:
+        if value < min_value:
+            min_value = value
+    result = min_value
+    if result == 1:
+        raise ValueError('nan')
+    return result
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
