9f75f2ea151474b5f02f9d38400160e0e4a83490
slowy07 <slowy.arfy@gmail.com>
2023-03-13 18:44:49 +0700

    fix: mengurangi kompleksitas kode algoritma cari kata
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- algorithm/backtracking/cari_kata.py
+++ algorithm/backtracking/cari_kata.py
@@ -6,84 +6,92 @@
 # https://leetcode.com/problems/word-search/
 
 
-def kata_ada(board: list[list[str]], kata: str) -> bool:
+def dapatkan_kata_kunci(
+    panjang_papan: int, panjang_kolom_papan: int, baris: int, kolom: int
+) -> int:
     """
-    >>> kata_ada([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")
-    True
-    >>> kata_ada([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCB")
+    >>> dapatkan_kata_kunci(10, 20, 1, 0)
+    200
+    """
+    return panjang_papan * panjang_kolom_papan * baris + kolom
+
+
+def cari_kata(
+    papan: list[list[str]],
+    kata: str,
+    baris: int,
+    kolom: int,
+    indeks_kata: int,
+    set_poin: set[int],
+) -> bool:
+    """
+    >>> cari_kata([["A"]], "B", 0, 0, 0, set())
     False
     """
-    # memvalidasi matriks
-    board_error_message = (
-        "matriks harus berupa string karakter tunggal yang tidak kosong"
-    )
-    if not isinstance(board, list) or len(board) == 0:
-        raise ValueError(board_error_message)
+    if papan[baris][kolom] != kata[indeks_kata]:
+        return False
+    if indeks_kata == len(kata) - 1:
+        return True
 
-    for row in board:
-        if not isinstance(row, list) or len(row) == 0:
-            raise ValueError(board_error_message)
+    traverts_diversion = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+    panjang_papan = len(papan)
+    panjang_kolom_papan = len(papan[0])
+    for direction in traverts_diversion:
+        next_i = baris + direction[0]
+        next_j = kolom + direction[1]
+        if not (0 <= next_i < panjang_papan and 0 <= next_j < panjang_kolom_papan):
+            continue
 
-        for item in row:
-            if not isinstance(item, str) or len(item) != 1:
-                raise ValueError(board_error_message)
+        kunci = dapatkan_kata_kunci(panjang_papan, panjang_kolom_papan, next_i, next_j)
+        if kunci in set_poin:
+            continue
 
-    # memvalidasi kata
-    if not isinstance(kata, str) or len(kata) == 0:
-        raise ValueError(
-            "parameter kata harus berupa string dengan panjang lebih dari 0"
-        )
-
-    traverts_directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
-    len_kata = len(kata)
-    len_board = len(board)
-    len_board_column = len(board[0])
-
-    def dapatkan_kunci_poin(row: int, kolom: int) -> int:
-        """
-        >>> len_board=10
-        >>> len_board_column=20
-        >>> dapatkan_kunci_poin(0, 0)
-        200
-        """
-        return len_board * len_board_column * row + kolom
-
-    def kata_sudah_ada(
-        row: int, kolom: int, index_kata: int, poin_kunjungan: set[int]
-    ) -> bool:
-        if board[row][kolom] != kata[index_kata]:
-            return False
-
-        if index_kata == len_kata - 1:
+        set_poin.add(kunci)
+        if cari_kata(papan, kata, next_i, next_j, indeks_kata + 1, set_poin):
             return True
 
-        for direction in traverts_directions:
-            next_i = row + direction[0]
-            next_j = kolom + direction[1]
-            if not (0 <= next_i < len_board and 0 <= next_j < len_board_column):
-                continue
+        set_poin.remove(kunci)
 
-            key = dapatkan_kunci_poin(next_i, next_j)
-            if key in poin_kunjungan:
-                continue
+    return False
 
-            poin_kunjungan.add(key)
-            if kata_sudah_ada(next_i, next_j, index_kata + 1, poin_kunjungan):
+
+def cek_kata(papan: list[list[str]], kata: str) -> bool:
+    """
+    >>> cek_kata([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")
+    True
+    """
+    pesan_error = "papan tidak boleh kosong,"
+    panjang_papan = len(papan)
+    if not isinstance(papan, list) or len(papan) == 0:
+        raise ValueError(pesan_error)
+
+    for baris in papan:
+        if not isinstance(baris, list) or len(baris) == 0:
+            raise ValueError(pesan_error)
+
+        for item in baris:
+            if not isinstance(item, str) or len(item) != 1:
+                raise ValueError(pesan_error)
+
+    if not isinstance(kata, str) or len(kata) == 0:
+        raise ValueError("parameter kata tidak boleh kosong")
+
+    panjang_kolom_papan = len(papan[0])
+    for i in range(panjang_papan):
+        for j in range(panjang_kolom_papan):
+            if cari_kata(
+                papan,
+                kata,
+                i,
+                j,
+                0,
+                {dapatkan_kata_kunci(panjang_papan, panjang_kolom_papan, i, j)},
+            ):
                 return True
-
-            poin_kunjungan.remove(key)
-
-        return False
-
-    for i in range(len_board):
-        for j in range(len_board_column):
-            if kata_sudah_ada(i, j, 0, {dapatkan_kunci_poin(i, j)}):
-                return True
-
     return False
 
 
 if __name__ == "__main__":
     import doctest
 
-    doctest.testmod()
+    doctest.tesmod()
