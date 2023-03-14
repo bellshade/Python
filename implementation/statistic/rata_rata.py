f38fc5e2e6ba0c7c11f9d9b192eb5d90f6cf00f6
Iqbal Ramadhan Anniswa <95537097+kayabaakihiko13@users.noreply.github.com>
2023-03-14 12:52:15 +0700

    feat:Menambahkan algoritma mean,media,modus (#245)
    * feat:Menambahkan algoritma mean,media,modus
    
    * feat: menambahkan algoritma rata-rata,media dan modus
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ implementation/statistic/rata_rata.py
@@ -0,0 +1,34 @@
+def rata_rata(arr: list[int | float]) -> float:
+    """
+    rata-rata adalah menghitung seluruh nilai pada data dengan
+    panjangnya data tersebut
+
+    **Parameter**
+
+    ----
+    :param arr:parameter ini bertipe data list mengandung bisa berupa bulat dan desimal
+
+    **Parameter**
+
+    ----
+    **Return**
+    :param return: paramater ini beroutput angka desimal
+    **Contoh**
+    >>> data=[1,1,2,2,2,3,3]
+    >>> rata_rata(data)
+    2.0
+    """
+    arr = arr.copy()
+    panjang_data = 0
+    perjumlahan = 0
+    for value in arr:
+        panjang_data += 1
+        perjumlahan += value
+    hasil = perjumlahan / panjang_data
+    return hasil
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
