41a8b4f7a173864eefeeb56d2a477463deec2a5f
kayabaakihiko13 <iqbalramad75@gmail.com>
2023-03-19 15:11:09 +0700

    feat: menambahkan algoritma variance,entropy dan information gain

---
+++ implementation/statistic/entropy.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+def entropy(labels , base=None) -> float:
+    """
+    menghitung entropy dari suatu fitur pada suatu dataset.
+
+    Parameter:
+
+    -----------
+    labels:numpy.array
+        Array 1-D yang berisi fitur yang akan dihitung entropynya
+    base: int
+        base ini merupakan basis log yang diingikan oleh pemakai
+    refensi:
+    https://www.youtube.com/watch?v=2s3aJfRr9gE (Video Tutorial)
+    https://en.wikipedia.org/wiki/Entropy_(information_theory) (online reading)
+    https://towardsdatascience.com/entropy-how-decision-trees-make-decisions-2946b9c18c8
+    (Towards Data Science)
+
+    example:
+
+    --------
+    >>> label=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    >>> entropy(label,base=2)
+    0.9967916319816366
+    """
+    _ , counts = np.unique(labels , return_counts=True)
+    probs = counts / len(labels)
+    logs = np.log(probs) if base is None else np.log(probs) / np.log(base)
+    return np.sum(abs(probs * logs))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
---
+++ implementation/statistic/variance.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def variance(arr: np.array , ddof=0) -> float:
+    """
+    variance adalah untuk mengetahui keragaraman data terhadap selisih dari
+    rata-rata tersebut
+
+    **Params**
+    ----------
+    arr:parameter ini berfungsi untuk input data
+
+    **Return**
+
+    ----------
+    float:hasil dari operasi ini berbentuk bilangan desimal
+    """
+    banyak_data = len(arr) - ddof
+    formula = sum((arr - arr.mean()) ** 2) / banyak_data
+
+    return formula
+if __name__=="__main__":
+    import doctest
+    doctest.testmod(verbose=True)
