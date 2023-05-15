3c8efd5091bd54a97dd199dd8061491d0e3b91c5
kayabaakihiko13 <iqbalramad75@gmail.com>
2023-05-15 21:54:08 +0700

    feat: penambahan mean absolute percentage error dan update dokumentasi

--- implementation/artificial_intelligence/fungsi_skoring.py
+++ implementation/artificial_intelligence/fungsi_skoring.py
@@ -14,6 +14,8 @@
 #   menambahkan satu ke aktual sebelum mengambil
 #   nilai logaritma natural untuk menghindari
 #   catching log natural nilai nol
+# - MAPE (Mean Absoult Prediction Error)
+#   fungsi ini untuk mengukur pesentasi akurasi method forecasting
 
 import numpy as np
 
@@ -82,12 +84,57 @@ def fungsi_rmsle(prediksi: list, nilai_aktual: list) -> float:
 
 
 def fungsi_mbd(prediksi: list, nilai_aktual: list) -> float:
+    """
+    Deskripsi:
+    ---
+    mean bias deviation adalah merupakan
+    pengukuran untuk presentasi kualitas
+    karateristik model
+
+    Parameters:
+    ---
+    :param prekdisi: parameter berisi nilai prediksi
+    :param aktual: parameter berisi nilai prediksi
+    """
     prediksi = np.array(prediksi)
     nilai_aktual = np.array(nilai_aktual)
     perbedaan = prediksi - nilai_aktual
     numerator = np.sum(perbedaan) / len(prediksi)
     denumerator = np.sum(nilai_aktual) / len(prediksi)
     hasil = float(numerator) / denumerator * 100
+    return hasil
+
+
+def fungsi_mape(prediksi: list, nilai_aktual: list) -> float:
+    """
+    Deskripsi:
+    ---
+    Mean Absolute Percentage Error (MAPE) merupakan
+    pengukuran akurasi nilai prediksi untuk
+    metode forcesting
+    Paramaters:
+    ---
+    :param prediksi: parameter berisi nilai prediksi
+    :param aktual: paramter berisi nilai aktual
+
+    Example:
+    ---
+    >>> nilai_aktual=[12,10,11]
+    >>> nilai_prediksi=[12,9,12]
+    >>> fungsi_mape(nilai_prediksi,nilai_aktual)
+    6.363636363636364
+    Refensi:
+    ---
+    Time Series Forecasting in Python hal.48
+    https://en.wikipedia.org/wiki/Mean_absolute_percentage_error
+    """
+    nilai_aktual = np.array(nilai_aktual)
+    nilai_prediksi = np.array(prediksi)
+    # numertor pada MAPE dimana absolute
+    # selesih nilai aktual sama nilai prediksi
+    # dibagi dengan nilai prediksi tersebut
+    numerator = np.abs((nilai_aktual - nilai_prediksi) / nilai_aktual)
+    hasil = np.mean(numerator) * 100
     return hasil
 
 
