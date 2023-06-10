eb39f3c00c781534518b10a358955d846e9ca872
slowy07 <slowy.arfy@gmail.com>
2023-06-10 12:08:00 +0700

    fix: memperbaiki beberapa kode yang terbaca warning dan error pada LGTM security
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- struktur_data/tree/binary_search_tree/binary_search_tree.py
+++ struktur_data/tree/binary_search_tree/binary_search_tree.py
@@ -1,11 +1,14 @@
+from typing import Optional
+
+
 class BinarySearchTree:
     # method __init__ digunakan untuk membangun Node
-    def __init__(self, data: int = None):
-        self.data = data
-        self.left = None
-        self.right = None
+    def __init__(self, data: Optional[int] = None) -> None:
+        self.data: Optional[int] = data
+        self.left: Optional[int] = None
+        self.right: Optional[int] = None
 
-    def insert(self, new_data):
+    def insert(self, new_data: int) -> None:
         if self.data is None:
             # menginisasi root jika belum ada node sama sekali
             self.data = new_data
@@ -24,40 +27,46 @@ class BinarySearchTree:
                 # rekursif melalui atribut right
                 self.right.insert(new_data)
 
-    def preorder(self, arr: list = []):
+    def preorder(self, arr: Optional[list[int]] = None) -> list[int]:
         # Algoritma preorder adalah sebagai berikut:
         # 1. Append data pada node
         # 2. Traverse ke kiri
         # 3. Traverse ke kanan
-        arr.append(self.data)  # lgtm [py/modification-of-default-value]
+        if arr is None:
+            arr = []
+        arr.append(self.data)
         if self.left is not None:
             self.left.preorder(arr)
         if self.right is not None:
             self.right.preorder(arr)
         return arr
 
-    def inorder(self, arr: list = []):
+    def inorder(self, arr: Optional[list[int]] = None) -> list[int]:
         # Algoritma inorder adalah sebagai berikut:
         # 1. Traverse ke kiri
         # 2. Append data pada node
         # 3. Traverse ke kanan
-        if self.left is not None:
+        if arr is None:
+            arr = []
+        if self.left:
             self.left.inorder(arr)
-        arr.append(self.data)  # lgtm [py/modification-of-default-value]
-        if self.right is not None:
+        arr.append(self.data)
+        if self.right:
             self.right.inorder(arr)
         return arr
 
-    def postorder(self, arr: list = []):
+    def postorder(self, arr: Optional[list[int]] = None) -> list[int]:
         # Algoritma postorder adalah sebagai berikut:
         # 1. Traverse ke kiri
         # 2. Traverse ke kanan
         # 3. Append data pada node
-        if self.left is not None:
+        if arr is None:
+            arr = []
+        if self.left:
             self.left.postorder(arr)
-        if self.right is not None:
+        if self.right:
             self.right.postorder(arr)
-        arr.append(self.data)  # lgtm [py/modification-of-default-value]
+        arr.append(self.data)
         return arr
 
 
