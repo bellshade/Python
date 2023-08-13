d1a671549fbfa47eb90f24a8d3ea9aba9d03f016
Iqbal Ramadhan Anniswa <95537097+kayabaakihiko13@users.noreply.github.com>
2023-08-14 06:53:55 +0700

    feat: menambah exponential (#298)
    * feat: menambah exponential
    
    ``Deskripsi``
    Exponent merupakan fungsi matematika yang pangkat nya
    berbasis nilai eular
    
    ``Refence``
    [wikipedia](https://en.wikipedia.org/wiki/Exponential_function)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update exponential_function.py
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ math/exponential_function.py
@@ -0,0 +1,29 @@
+from math import factorial
+
+
+def exponent(x: int | float):
+    """_summary_
+
+    Args:
+        x (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    Contoh:
+    >>> exponent(2)
+    7.38905609893
+    >>> exponent(1)
+    2.71828182846
+    """
+    n_term: int = 100
+    result: float = 0.0
+    for n in range(n_term):
+        result += x**n / factorial(n)
+
+    return round(result, 11)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
