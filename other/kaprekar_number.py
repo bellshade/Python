aaeedbf0c2ccb05a3880035bfc76860fc1b78db7
Arlan Tengga <arlantengga8@gmail.com>
2025-10-20 21:34:02 +0700

    feat: menambahkan algoritma kaprekar number

---
+++ other/kaprekar_number.py
@@ -0,0 +1,60 @@
+# mengecek apakah bilangan apakah bilangan itu masuk kaprekar
+# https://en.wikipedia.org/wiki/Kaprekar_number
+import numpy as np
+
+
+def kaprekar_number(number):
+    """
+    Angka kaprekar adalah angka yang yang di kuadratkan,
+    dan di pisahmenjadi dua bagian dan di jumlahkan lagi
+    maka akat menjadi angka itu kembali.
+
+    >>> kaprekar_number(45)
+    'Angka Kaprekar'
+    >>> kaprekar_number(9)
+    'Angka Kaprekar'
+    >>> kaprekar_number(5)
+    'Bukan Angka Kaprekar'
+    >>> kaprekar_number(4)
+    'Bukan Angka Kaprekar'
+
+    """
+    benar , bukan = "Angka Kaprekar" , "Bukan Angka Kaprekar"
+    if number == 1:
+        return benar
+
+    square_number = number * number
+
+    cout_d = 0
+    while not square_number == 0:
+        cout_d += 1
+        square_number = square_number // 10
+
+    square_number = number * number
+
+    r_digits = 0
+    while r_digits < cout_d:
+        r_digits += 1
+        eq_parts = np.pow(10 , r_digits)
+
+        sum = square_number // eq_parts + square_number % eq_parts
+        if sum == number:
+            return benar
+
+    return bukan
+
+
+def main(args=None):
+    import doctest
+
+    doctest.testmod()
+
+    # sampel case
+    print(kaprekar_number(45))  # Angka Kaprekar
+    print(kaprekar_number(9))  # Angka Kaprekar
+    print(kaprekar_number(5))  # Bukan Angka Kaprekar
+    print(kaprekar_number(10))  # Bukan Angka Kaprekar
+
+
+if __name__ == "__main__":
+    main()
