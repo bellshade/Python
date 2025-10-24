2b41154ef56009e9c00b49b4172b26af7d4cfe11
Arlan Tengga <arlantengga8@gmail.com>
2025-10-24 21:00:44 +0700

    feat: menambahkan algoritma krisnamurty

---
+++ other/krisnamurtyCheck.py
@@ -0,0 +1,50 @@
+# Mengecek apakah krisnamurty atau bukan
+from math import factorial
+
+
+def krisnamurty(number : int) -> int | str:
+    """
+    Angka Krisnamurty adalah angka factorial yang setiap digitnya
+    sama dengan factorial nya.
+
+    contoh :
+    145! = 1! + 4! + 5! -> Angka Krisnamurty
+
+    >>> krisnamurty(145)
+    'Angka Krisnamurty'
+    >>> krisnamurty(1)
+    'Angka Krisnamurty'
+    >>> krisnamurty(123)
+    'Bukan Angka Krisnamuty'
+
+    """
+    error = "Masukkan Angka dengan benar"
+    benar , bukan = "Angka Krisnamurty" , "Bukan Angka Krisnamuty"
+    if number <= 0:
+        return error
+    else :
+        temp = number
+        total = 0
+        while temp > 0:
+            digit = temp % 10
+            total += factorial(digit)
+            temp //= 10
+        if total == number:
+            return benar
+        else :
+            return bukan
+
+
+def main(args=None):
+    import doctest
+
+    doctest.testmod()
+
+    # sample case
+    print(krisnamurty(145))  # Angka Krisnamurty
+    print(krisnamurty(1))  # Angka Krisnamurty
+    print(krisnamurty(123))  # Bukan Angka Krisnamuty
+
+
+if __name__ == "__main__":
+    main()
