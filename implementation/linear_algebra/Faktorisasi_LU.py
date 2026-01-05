ee6c342ad9220daf460e320b1c1a3301bcb541d3
Arlan Tengga <arlantengga8@gmail.com>
2026-01-05 11:11:22 +0700

    feat: menambahkan faktorisasi LU

---
+++ implementation/linear_algebra/Faktorisasi_LU.py
@@ -0,0 +1,40 @@
+# https://en.wikipedia.org/wiki/LU_decomposition#Algorithms
+
+from typing import Union
+
+
+def LU(A : list[list[Union[int, float]]]) -> list[list[Union[int, float]]]:
+    """
+    >>> A = [[2, 3, 1], [4, 7, 7], [-2, 4, 5]]
+    >>> L, U = LU(A)
+    >>> L
+    [[1, 0, 0], [2.0, 1, 0], [-1.0, 7.0, 1]]
+    >>> U
+    [[2, 3, 1], [0.0, 1.0, 5.0], [0.0, 0.0, -29.0]]
+    """
+    n = len(A)
+    L = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
+    U = [[A[i][j] for j in range(n)] for i in range(n)]
+
+    for k in range(n - 1):
+        if U[k][k] == 0:
+            raise ValueError("Faktorisasi LU tidak ada")
+        for i in range(k + 1, n):
+            L[i][k] = U[i][k] / U[k][k]
+            for j in range(k, n):
+                U[i][j] -= L[i][k] * U[k][j]
+    return L, U
+
+
+def main(args=None):
+    import doctest
+
+    doctest.testmod()
+    A = [[2, 3, 1], [4, 7, 7], [-2, 4, 5]]
+    L, U = LU(A)
+    print(L)
+    print(U)
+
+
+if __name__ == "__main__":
+    main()
